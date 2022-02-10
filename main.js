803d0fd64242c88e8707604589b6014fc5969a58
Abbi Engel <abbi.engel33@gmail.com>
2022-02-10 13:51:15 -0600

    do while error

--- main.js
+++ main.js
@@ -26,13 +26,17 @@ function display(projects) {
 }
 
 async function main() { 
-    try {
-    const resp = await input.text('What is your GitHub username?')
-    const repos = await github(resp);
-    display(repos);
-        } catch (error) {
-    console.log(error.message);
-    }
+    let err;
+    do { 
+        try {
+            const resp = await input.text('What is your GitHub username?')
+            const repos = await github(resp);
+            display(repos);
+            } catch (error) {
+                console.log(error.message);
+            err = error;
+        }
+    } while (err != undefined);
 }
 
 main();
